2b947f8164412c5eb77a2fd2ca97a17ad8fd7e0c
unknown <gktme23@gmail.com>
2023-12-07 01:18:07 +0200

    add Required attrubute to prop

--- ToDoApp.Domain/Models/ToDoTask.cs
+++ ToDoApp.Domain/Models/ToDoTask.cs
@@ -1,9 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
 ﻿using ToDoApp.Domain.Models.Base;
 
 namespace ToDoApp.Domain.Models
 {
     public class ToDoTask : EntityBase
     {
+        [Required]
         public string Title { get; set; }
         public bool IsCompleted { get; set; }
         public DateTimeOffset DateCreated { get; set; }
